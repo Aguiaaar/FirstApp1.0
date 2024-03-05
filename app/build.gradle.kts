3e3863cc0a998a0f63b84e52531fcf606d5dce70
Ana Laura Aguiar Gonçales <ana.goncales@portalsesisp.org.br>
2024-03-05 15:27:47 -0300

    criar um layout para calcular a idade. criar um layout para validar um email e telefone.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.analauraaguiar.firstapp10"
-        minSdk = 24
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
